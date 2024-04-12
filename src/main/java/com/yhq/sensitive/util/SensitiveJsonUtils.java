1d341534aa47e3f0e6342e10b95df9ce01cd8319
nuccch2010 <nuccch2010@163.com>
2024-04-12 09:18:37 +0800

    JSON库检查优化

--- src/main/java/com/yhq/sensitive/util/SensitiveJsonUtils.java
+++ src/main/java/com/yhq/sensitive/util/SensitiveJsonUtils.java
@@ -5,6 +5,7 @@ import com.yhq.sensitive.core.GsonOutput;
 import com.yhq.sensitive.core.JacksonOutput;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.util.ClassUtils;
 
 /**
  * 脱敏信息JSON序列化
@@ -14,9 +15,20 @@ import org.slf4j.LoggerFactory;
  */
 public class SensitiveJsonUtils {
     private static final Logger LOGGER = LoggerFactory.getLogger(SensitiveJsonUtils.class);
-    private static final String JACKSON = "com.fasterxml.jackson.core.JsonGenerator";
-    private static final String FASTJSON = "com.alibaba.fastjson.JSON";
-    private static final String GSON = "com.google.gson.Gson";
+    // 存在jackson库
+    private static final boolean jacksonPresent =
+            ClassUtils.isPresent("com.fasterxml.jackson.databind.ObjectMapper",
+                    SensitiveJsonUtils.class.getClassLoader()) &&
+            ClassUtils.isPresent("com.fasterxml.jackson.core.JsonGenerator",
+                    SensitiveJsonUtils.class.getClassLoader());
+    // 存在fastjson库
+    private static final boolean fastjsonPresent =
+            ClassUtils.isPresent("com.alibaba.fastjson.JSON",
+                    SensitiveJsonUtils.class.getClassLoader());
+    // 存在gson库
+    private static final boolean gsonPresent =
+            ClassUtils.isPresent("com.google.gson.Gson",
+                    SensitiveJsonUtils.class.getClassLoader());
 
     /**
      * 将对象转换为JSON字符串，如果对象属性中使用了脱敏注解，在转换时会进行脱敏处理
@@ -24,34 +36,19 @@ public class SensitiveJsonUtils {
      * @return
      */
     public static String toJson(Object object) {
-        if (checkClassExists(JACKSON)) {
+        if (jacksonPresent) {
             // 优先使用jackson进行序列化
             return JacksonOutput.nonNullMapper().toJson(object);
-        } else if (checkClassExists(FASTJSON)) {
+        } else if (fastjsonPresent) {
             // 使用FastJson序列化
             return FastJsonOutput.getInstance().toJson(object);
-        } else if (checkClassExists(GSON)) {
+        } else if (gsonPresent) {
             // 使用Gson序列化
             return GsonOutput.getInstance().toJson(object);
         } else {
             LOGGER.error("Can not find any JSON serializer! Please add some such as jackson, fastjson or gson");
         }
         return null;
-    }
-
-    /**
-     * 检查指定类是否存在
-     * @param clazz
-     * @return
-     */
-    private static boolean checkClassExists(String clazz) {
-        try {
-            Class.forName(clazz, false, SensitiveJsonUtils.class.getClassLoader());
-            return true;
-        } catch (ClassNotFoundException e) {
-            LOGGER.warn("{} not found", clazz);
-        }
-        return false;
     }
 
 }
